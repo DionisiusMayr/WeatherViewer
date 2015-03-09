f2fa4b9420725c5ce25b631ed84ed0d714d1860b
Thomas Lui <tlfit28@gmail.com>
2015-03-09 00:38:20 -0400

    Update LocalWeatherView.java

--- src/main/java/ca/uwo/csd/cs2212/team4/LocalWeatherView.java
+++ src/main/java/ca/uwo/csd/cs2212/team4/LocalWeatherView.java
@@ -36,48 +36,62 @@ public class LocalWeatherView {
 		return main.optString("pressure", null);
 	}
 
+      /** getHumidity method to return humidity
+ */
 	public static String getHumidity() throws JSONException, IOException {
 		JSONObject object = data.createJsonObject(data.getContentOfURL());
 		JSONObject main = object.optJSONObject("main");
 		return main.optString("humidity", null);
 	}
 
+ /** getTempMin method to return min Temp
+ */
+
+
 	public static String getTempMin() throws JSONException, IOException {
 		JSONObject object = data.createJsonObject(data.getContentOfURL());
 		JSONObject main = object.optJSONObject("main");
 		return main.optString("temp_min", null);
 	}
-
+ /** getTempMax method to return Max temp
+ */
 	public static String getTempMax() throws JSONException, IOException {
 		JSONObject object = data.createJsonObject(data.getContentOfURL());
 		JSONObject main = object.optJSONObject("main");
 		return main.optString("temp_max", null);
 	}
 
+ /** getWindSpeed method to return wind speed
+ */
+
 	public static String getWindSpeed() throws JSONException, IOException {
 		JSONObject object = data.createJsonObject(data.getContentOfURL());
 		JSONObject wind = object.optJSONObject("wind");
 		return wind.optString("speed", null);
 	}
-
+ /** getWindDirection method to return wind direction
+ */
 	public static String getWindDirection() throws JSONException, IOException {
 		JSONObject object = data.createJsonObject(data.getContentOfURL());
 		JSONObject wind = object.optJSONObject("wind");
 		return wind.optString("deg", null);
 	}
-
+ /** getSunrise method to return sunrise time
+ */
 	public static String getSunrise() throws JSONException, IOException {
 		JSONObject object = data.createJsonObject(data.getContentOfURL());
 		JSONObject sys = object.optJSONObject("sys");
 		return sys.optString("sunrise", null);
 	}
-
+ /** getSunset method to return sunset time
+ */
 	public static String getSunset() throws JSONException, IOException {
 		JSONObject object = data.createJsonObject(data.getContentOfURL());
 		JSONObject sys = object.optJSONObject("sys");
 		return sys.optString("sunset", null);
 	}
-
+ /** getSkyCondition method to return Sky condition
+ */
 	public static String getSkyCondition() throws JSONException, IOException {
 		JSONObject object = data.createJsonObject(data.getContentOfURL());
 		JSONArray weatherArray = object.optJSONArray("weather");
