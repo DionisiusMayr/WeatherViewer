1b5fb18fc8c108028f8423e9c9d6572f7f8fe4c7
gym1 <lawrenceyiming@gmail.com>
2015-03-28 14:00:37 -0300

    Update GUILongTermWeather.java

--- Users/Charlie/src/main/java/ca/uwo/csd/cs2212/team4/GUILongTermWeather.java
+++ Users/Charlie/src/main/java/ca/uwo/csd/cs2212/team4/GUILongTermWeather.java
@@ -1,102 +1,551 @@
 package ca.uwo.csd.cs2212.team4;
 
-import java.awt.Dimension;
-import java.awt.EventQueue;
-
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.*;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.StringTokenizer;
 
-import java.awt.*;
+import org.json.JSONException;
 
+import java.io.IOException;
 
-public class GUILongTermWeather extends JFrame{
-
-	private JTable table;
+public class GUILongTermWeather extends JFrame implements ActionListener {
 	private JPanel contentPane;
+	private String city,country;
+	private LongTerm weather;
+	private Boolean nextDay, addHours;
+	private JLabel[] day,skyCondition,icon,date, max, min, rise, set;
 
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					GUILongTermWeather window = new GUILongTermWeather();
-//					window.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUILongTermWeather frame = new GUILongTermWeather();
+					frame.setVisible(true);
+				}
+                catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Create the frame.
 	 */
 	public GUILongTermWeather() {
-		this.setTitle("Long-Term Weather View"); 
-		this.setBounds(100, 100, 731, 454);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//city = GUIApp.pref.getCity();
+		//country = GUIApp.pref.getCountry();
+		nextDay=false;
+
+		setTitle("Long-Term Weather");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 465, 500);
 		contentPane = new JPanel();
+		contentPane.setForeground(Color.BLACK);
 		contentPane.setMinimumSize(new Dimension(650, 40));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		this.setContentPane(contentPane);
+
+		CustomLabel lbl1 = new CustomLabel("Day 1");
+		lbl1.setBounds(50, 20, 80, 20);
+		contentPane.add(lbl1);
+
+		CustomLabel lbl2 = new CustomLabel("Day 2");
+		lbl2.setBounds(150, 20, 80, 20);
+		contentPane.add(lbl2);
+
+		CustomLabel lbl3 = new CustomLabel("Day 3");
+		lbl3.setBounds(250, 20, 80, 20);
+		contentPane.add(lbl3);
+
+		CustomLabel lbl4 = new CustomLabel("Day 4");
+		lbl4.setBounds(350, 20, 80, 20);
+		contentPane.add(lbl4);
+
+		CustomLabel lbl5 = new CustomLabel("Day 5");
+		lbl5.setBounds(50, 240, 80, 20);
+		contentPane.add(lbl5);
+
+		CustomLabel lbl6 = new CustomLabel("Day 6");
+		lbl6.setBounds(150,240, 80, 20);
+		contentPane.add(lbl6);
+
+		CustomLabel lbl7 = new CustomLabel("Day 7");
+		lbl7.setBounds(250, 240, 80, 20);
+		contentPane.add(lbl7);
+
+
+		CustomLabel tempday1 = new CustomLabel("day");
+		tempday1.setBounds(50, 160, 80, 14);
+		contentPane.add(tempday1);
+		tempday1.setText("day");
+
+		CustomLabel tempday2 = new CustomLabel("day");
+		tempday2.setBounds(150, 160, 80, 14);
+		contentPane.add(tempday2);
+		tempday2.setText("day");
+
+		CustomLabel tempday3 = new CustomLabel("day");
+		tempday3.setBounds(250, 160, 80, 14);
+		contentPane.add(tempday3);
+		tempday3.setText("day");
+
+		CustomLabel tempday4 = new CustomLabel("day");
+		tempday4.setBounds(350, 160, 80, 14);
+		contentPane.add(tempday4);
+		tempday4.setText("day");
+
+		CustomLabel tempday5 = new CustomLabel("day");
+		tempday5.setBounds(50, 375, 80, 14);
+		contentPane.add(tempday5);
+		tempday5.setText("day");
+
+		CustomLabel tempday6 = new CustomLabel("day");
+		tempday6.setBounds(150, 375, 80, 14);
+		contentPane.add(tempday6);
+		tempday6.setText("day");
+
+		CustomLabel tempday7 = new CustomLabel("day");
+		tempday7.setBounds(250, 375, 80, 14);
+		contentPane.add(tempday7);
+		tempday7.setText("day");
+
+		//skycondition
+		CustomLabel skyCon1 = new CustomLabel("day");
+		skyCon1.setBounds(50, 145, 80, 14);
+		contentPane.add(skyCon1);
+		skyCon1.setText("skyCon");
+
+		CustomLabel skyCon2 = new CustomLabel("day");
+		skyCon2.setBounds(150, 145, 80, 14);
+		contentPane.add(skyCon2);
+		skyCon2.setText("skyCon");
+
+		CustomLabel skyCon3 = new CustomLabel("day");
+		skyCon3.setBounds(250, 145, 80, 14);
+		contentPane.add(skyCon3);
+		skyCon3.setText("skyCon");
+
+		CustomLabel skyCon4 = new CustomLabel("day");
+		skyCon4.setBounds(350, 145, 80, 14);
+		contentPane.add(skyCon4);
+		skyCon4.setText("skyCon");
+
+		CustomLabel skyCon5 = new CustomLabel("day");
+		skyCon5.setBounds(50, 360, 80, 14);
+		contentPane.add(skyCon5);
+		skyCon5.setText("skyCon");
+
+		CustomLabel skyCon6 = new CustomLabel("day");
+		skyCon6.setBounds(150, 360, 80, 14);
+		contentPane.add(skyCon6);
+		skyCon6.setText("skyCon");
+
+		CustomLabel skyCon7 = new CustomLabel("day");
+		skyCon7.setBounds(250, 360, 80, 14);
+		contentPane.add(skyCon7);
+		skyCon7.setText("skyCon");
+
+
+		//sky condition icons
+		JLabel sImg1 = new JLabel(new ImageIcon("snow.png"));
+		sImg1.setBounds(25,40,100,100);
+		contentPane.add(sImg1);
+
+		JLabel sImg2 = new JLabel(new ImageIcon("snow.png"));
+		sImg2.setBounds(125,40,100,100);
+		contentPane.add(sImg2);
+
+		JLabel sImg3 = new JLabel(new ImageIcon("snow.png"));
+		sImg3.setBounds(225,40,100,100);
+		contentPane.add(sImg3);
+
+		JLabel sImg4 = new JLabel(new ImageIcon("snow.png"));
+		sImg4.setBounds(325,40,100,100);
+		contentPane.add(sImg4);
+
+		JLabel sImg5 = new JLabel(new ImageIcon("snow.png"));
+		sImg5.setBounds(25,255,100,100);
+		contentPane.add(sImg5);
+
+		JLabel sImg6 = new JLabel(new ImageIcon("snow.png"));
+		sImg6.setBounds(125,255,100,100);
+		contentPane.add(sImg6);
+
+		JLabel sImg7 = new JLabel(new ImageIcon("snow.png"));
+		sImg7.setBounds(225,255,100,100);
+		contentPane.add(sImg7);
+
 		
-		table = new JTable();
-		table.setShowHorizontalLines(false);
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
- 				//7days per a Week from Monday to Sunday
-				{null, "MO", "TU", "WED", "TH", "FR", "SA", "SU"},
-				//Need to have a getter method to get the weather conditions from website
-				{"Weather Condition", null, null, null, null, null, null, null},
-				//It is the picture that symbolizes each weather conditions
-				{null, getpicture(), null, null, null, null, null, null},
-				//Need to have a getter method to get the weather Temperature from website
-				{"Temp", null, null, null, null, null, null, null},
-				//Need to have a getter method to get the weather Maximum Temperature from website
-				{"Max Temp", null, null, null, null, null, null, null},
-				////Need to have a getter method to get the weather Minimum Temperature from website
-				{"Min Temp", null, null, null, null, null, null, null},
-				//Need to have a getter method to get the weather humidity from website
-				{"Humidity", null, null, null, null, null, null, null},
-				//Need to have a getter method to get the wind speed from website
-				{"Wind Speed", null, null, null, null, null, null, null},
-				//Need to have a getter method to get the wind direction from website
-				{"Wind Direction", null, null, null, null, null, null, null},
-				//Need to have a getter method to get the time of sun rise from website
-				{"Sun Rise", null, null, null, null, null, null, null},
-				//Need to have a getter method to get the time of sun set from website
-				{"Sun Set", null, null, null, null, null, null, null},
-			},
-			new String[] {
-				"New column", "New column", "New column", "New column", "New column", "New column", "New column", "New column"
-			}
-		));
-		table.getColumnModel().getColumn(0).setPreferredWidth(128);
-		table.setBounds(10, 103, 695, 278);
-		contentPane.add(table);
+		//Max temp
+		CustomLabel maxday1 = new CustomLabel("max");
+		maxday1.setBounds(50, 175, 80, 14);
+		contentPane.add(maxday1);
+		maxday1.setText("max");
+
+		CustomLabel maxday2 = new CustomLabel("max");
+		maxday2.setBounds(150, 175, 80, 14);
+		contentPane.add(maxday2);
+		maxday2.setText("max");
+
+		CustomLabel maxday3 = new CustomLabel("max");
+		maxday3.setBounds(250, 175, 80, 14);
+		contentPane.add(maxday3);
+		maxday3.setText("max");
+
+		CustomLabel maxday4 = new CustomLabel("max");
+		maxday4.setBounds(350, 175, 80, 14);
+		contentPane.add(maxday4);
+		maxday4.setText("max");
+
+		CustomLabel maxday5 = new CustomLabel("max");
+		maxday5.setBounds(50, 390, 80, 14);
+		contentPane.add(maxday5);
+		maxday5.setText("max");
+
+		CustomLabel maxday6 = new CustomLabel("max");
+		maxday6.setBounds(150, 390, 80, 14);
+		contentPane.add(maxday6);
+		maxday6.setText("max");
+
+		CustomLabel maxday7 = new CustomLabel("max");
+		maxday7.setBounds(250, 390, 80, 14);
+		contentPane.add(maxday7);
+		maxday7.setText("max");
 		
-		JTextPane txtpnCurrentLocationLondon = new JTextPane();
-		txtpnCurrentLocationLondon.setEditable(false);
-		txtpnCurrentLocationLondon.setText("Current Location: London, ON, Canada");
-		txtpnCurrentLocationLondon.setBounds(27, 11, 230, 20);
-		contentPane.add(txtpnCurrentLocationLondon);
 		
-		JTextPane txtpnLastTimeUpgrade = new JTextPane();
-		txtpnLastTimeUpgrade.setText("Last Time Update: 9:31 am");
-		txtpnLastTimeUpgrade.setBounds(315, 11, 284, 20);
-		contentPane.add(txtpnLastTimeUpgrade);
+		//Min temp
+		CustomLabel minday1 = new CustomLabel("min");
+		minday1.setBounds(50, 190, 80, 14);
+		contentPane.add(minday1);
+		minday1.setText("min");
+
+		CustomLabel minday2 = new CustomLabel("min");
+		minday2.setBounds(150, 190, 80, 14);
+		contentPane.add(minday2);
+		minday2.setText("min");
+
+		CustomLabel minday3 = new CustomLabel("min");
+		minday3.setBounds(250, 190, 80, 14);
+		contentPane.add(minday3);
+		minday3.setText("min");
+
+		CustomLabel minday4 = new CustomLabel("min");
+		minday4.setBounds(350, 190, 80, 14);
+		contentPane.add(minday4);
+		minday4.setText("min");
+
+		CustomLabel minday5 = new CustomLabel("min");
+		minday5.setBounds(50, 405, 80, 14);
+		contentPane.add(minday5);
+		minday5.setText("min");
+
+		CustomLabel minday6 = new CustomLabel("min");
+		minday6.setBounds(150, 405, 80, 14);
+		contentPane.add(minday6);
+		minday6.setText("min");
+
+		CustomLabel minday7 = new CustomLabel("min");
+		minday7.setBounds(250, 405, 80, 14);
+		contentPane.add(minday7);
+		minday7.setText("min");
 		
+		
+		//Sun rise
+		CustomLabel riseday1 = new CustomLabel("rise");
+		riseday1.setBounds(50, 205, 80, 14);
+		contentPane.add(riseday1);
+		riseday1.setText("rise");
+
+		CustomLabel riseday2 = new CustomLabel("rise");
+		riseday2.setBounds(150, 205, 80, 14);
+		contentPane.add(riseday2);
+		riseday2.setText("rise");
+
+		CustomLabel riseday3 = new CustomLabel("rise");
+		riseday3.setBounds(250, 205, 80, 14);
+		contentPane.add(riseday3);
+		riseday3.setText("rise");
+
+		CustomLabel riseday4 = new CustomLabel("rise");
+		riseday4.setBounds(350, 205, 80, 14);
+		contentPane.add(riseday4);
+		riseday4.setText("rise");
+
+		CustomLabel riseday5 = new CustomLabel("rise");
+		riseday5.setBounds(50, 420, 80, 14);
+		contentPane.add(riseday5);
+		riseday5.setText("rise");
+
+		CustomLabel riseday6 = new CustomLabel("rise");
+		riseday6.setBounds(150, 420, 80, 14);
+		contentPane.add(riseday6);
+		riseday6.setText("rise");
+
+		CustomLabel riseday7 = new CustomLabel("rise");
+		riseday7.setBounds(250, 420, 80, 14);
+		contentPane.add(riseday7);
+		riseday7.setText("rise");	
+		
+		
+		//Sun set
+		CustomLabel setday1 = new CustomLabel("set");
+		setday1.setBounds(50, 220, 80, 14);
+		contentPane.add(setday1);
+		setday1.setText("set");
+
+		CustomLabel setday2 = new CustomLabel("set");
+		setday2.setBounds(150, 220, 80, 14);
+		contentPane.add(setday2);
+		setday2.setText("set");
+
+		CustomLabel setday3 = new CustomLabel("set");
+		setday3.setBounds(250, 220, 80, 14);
+		contentPane.add(setday3);
+		setday3.setText("set");
+
+		CustomLabel setday4 = new CustomLabel("set");
+		setday4.setBounds(350, 220, 80, 14);
+		contentPane.add(setday4);
+		setday4.setText("set");
+
+		CustomLabel setday5 = new CustomLabel("set");
+		setday5.setBounds(50, 435, 80, 14);
+		contentPane.add(setday5);
+		setday5.setText("set");
+
+		CustomLabel setday6 = new CustomLabel("set");
+		setday6.setBounds(150, 435, 80, 14);
+		contentPane.add(setday6);
+		setday6.setText("set");
+
+		CustomLabel setday7 = new CustomLabel("set");
+		setday7.setBounds(250, 435, 80, 14);
+		contentPane.add(setday7);
+		setday7.setText("set");	
+		
+		CustomLabel daytemp = new CustomLabel("Day");
+		daytemp.setBounds(2, 156, 80, 20);
+		contentPane.add(daytemp);
+		
+		CustomLabel daymax = new CustomLabel("Max");
+		daymax.setBounds(2, 171, 80, 20);
+		contentPane.add(daymax);
+		
+		CustomLabel daymin = new CustomLabel("Min");
+		daymin.setBounds(2, 186, 80, 20);
+		contentPane.add(daymin);
+		
+		CustomLabel dayrise = new CustomLabel("Rise");
+		dayrise.setBounds(2, 201, 80, 20);
+		contentPane.add(dayrise);
+		
+		CustomLabel dayset = new CustomLabel("Set");
+		dayset.setBounds(2, 216, 80, 20);
+		contentPane.add(dayset);
+		
+		CustomLabel daytemp1 = new CustomLabel("Day");
+		daytemp1.setBounds(2, 373, 80, 20);
+		contentPane.add(daytemp1);
+		
+		CustomLabel daymax1 = new CustomLabel("Max");
+		daymax1.setBounds(2, 388, 80, 20);
+		contentPane.add(daymax1);
+		
+		CustomLabel daymin1 = new CustomLabel("Min");
+		daymin1.setBounds(2, 403, 80, 20);
+		contentPane.add(daymin1);
+		
+		CustomLabel dayrise1 = new CustomLabel("Rise");
+		dayrise1.setBounds(2, 418, 80, 20);
+		contentPane.add(dayrise1);
+		
+		CustomLabel dayset1 = new CustomLabel("Set");
+		dayset1.setBounds(2, 433, 80, 20);
+		contentPane.add(dayset1);
+		
+		day = new CustomLabel[7];
+		day[0] = tempday1;
+		day[1] = tempday2;
+		day[2] = tempday3;
+		day[3] = tempday4;
+		day[4] = tempday5;
+		day[5] = tempday6;
+		day[6] = tempday7;
+
+
+		skyCondition = new CustomLabel[7];
+		skyCondition[0] = skyCon1;
+		skyCondition[1] = skyCon2;
+		skyCondition[2] = skyCon3;
+		skyCondition[3] = skyCon4;
+		skyCondition[4] = skyCon5;
+		skyCondition[5] = skyCon6;
+		skyCondition[6] = skyCon7;
+
+		
+		icon = new JLabel[7];
+		icon[0] = sImg1;
+		icon[1] = sImg2;
+		icon[2] = sImg3;
+		icon[3] = sImg4;
+		icon[4] = sImg5;
+		icon[5] = sImg6;
+		icon[6] = sImg7;
+		
+	
+		date = new CustomLabel[7];
+		date[0] = lbl1;
+		date[1] = lbl2;
+		date[2] = lbl3;
+		date[3] = lbl4;
+		date[4] = lbl5;
+		date[5] = lbl6;
+		date[6] = lbl7;
+		
+		
+		max = new CustomLabel[7];
+		max[0] = maxday1;
+		max[1] = maxday2;
+		max[2] = maxday3;
+		max[3] = maxday4;
+		max[4] = maxday5;
+		max[5] = maxday6;
+		max[6] = maxday7;
+		
+		
+		min = new CustomLabel[7];
+		min[0] = minday1;
+		min[1] = minday2;
+		min[2] = minday3;
+		min[3] = minday4;
+		min[4] = minday5;
+		min[5] = minday6;
+		min[6] = minday7;
+		
+		
+		rise = new CustomLabel[7];
+		rise[0] = riseday1;
+		rise[1] = riseday2;
+		rise[2] = riseday3;
+		rise[3] = riseday4;
+		rise[4] = riseday5;
+		rise[5] = riseday6;
+		rise[6] = riseday7;
+		
+		
+		set = new CustomLabel[7];
+		set[0] = setday1;
+		set[1] = setday2;
+		set[2] = setday3;
+		set[3] = setday4;
+		set[4] = setday5;
+		set[5] = setday6;
+		set[6] = setday7;
+		
+		try{
+		refresh(city,country);
+		}
+		catch(Exception e){
+			System.out.println("IO exception, Long term refresh method");
+		}
 	}
 
-	private Object getpicture() {
-		// TODO Auto-generated method stub
-		return null;
+	public void actionPerformed(ActionEvent ae) {
+		refresh(city);
+	}
+
+	public void refresh(String city){
+		try {
+            weather = new LongTerm(city);
+
+            for(int i = 0; i < 7; i++) {
+            	date[i].setText(dateParse(weather.getDate(i+1)));
+            	day[i].setText(weather.getTempDay(i));
+                skyCondition[i].setText(weather.getSkyCondition(i));
+                icon[i].setIcon(new ImageIcon(weather.getSkyCondition(i).toLowerCase() + ".png"));
+                max[i].setText(weather.getTempMax(i));
+                min[i].setText(weather.getTempMin(i));
+               // rise[i].setText(weather.getSunrise(i));
+               // set[i].setText(weather.getSunset(i));
+            }
+		}
+        catch(Exception e) {
+			System.out.println("IO exception, Long term refresh method");
+		}
+	}
+
+	public void refresh(String city,String country) throws IOException, JSONException{
+		weather = new LongTerm(city,country);
+
+		for(int i=0;i<7;i++) {
+			date[i].setText(dateParse(weather.getDate(i+1)));
+			day[i].setText(weather.getTempDay(i));
+			skyCondition[i].setText(weather.getSkyCondition(i));
+			icon[i].setIcon(new ImageIcon(weather.getSkyCondition(i).toLowerCase() + ".png"));
+            max[i].setText(weather.getTempMax(i));
+            min[i].setText(weather.getTempMin(i));
+            //rise[i].setText(weather.getSunrise(i));
+            //set[i].setText(weather.getSunset(i));
+		}
+	}
+
+
+	private String dateParse(String date) {
+		StringTokenizer st = new StringTokenizer(date);
+		String day = st.nextToken();
+		st.nextToken();
+		st.nextToken();
+		//String time = st.nextToken().substring(0, 3)+ "00";		
+		return (day);
 	}
 	
+	//not needed but maybe useful for long term
+    /* private String addHours(String time,int i)
+     {
+		//i=i-1;
+		System.out.println(i);
+		int temp = Integer.parseInt(time);
+		if(temp>=24){
+			temp = temp-24;
+			nextDay=true;
+		}
+		if(temp>=10){
+			return Integer.toString(temp);
+		}
+		return "0" + Integer.toString(temp);
+	}*/
+	
+	private String nextDay(String day){
+		if(day.compareTo("Mon")==0){
+			return "Tue";
+		}else if(day.compareTo("Tue")==0){
+			return "Wed";
+		}else if(day.compareTo("Wed")==0){
+			return "Thu";
+		}else if(day.compareTo("Thu")==0){
+			return "Fri";
+		}else if(day.compareTo("Fri")==0){
+			return "Sat";
+		}else if(day.compareTo("Sat")==0){
+			return "Sun";
+		}
+		return "Mon";
+		
+	}
 	public JPanel getPanel(){
 		return contentPane;
 	}
