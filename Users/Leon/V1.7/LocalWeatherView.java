ee0612186ca82a8baeb81454413618bc6079a6c7
Leon Meyer <lmeyer4@uwo.ca>
2015-03-28 21:24:32 -0400

    some updates, resources moved into resource folder to work. images working font not working

---
+++ Users/Leon/V1.7/LocalWeatherView.java
@@ -0,0 +1,182 @@
+package ca.uwo.csd.cs2212.team4;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Date;
+
+/**
+ * This class is for Local Weather View
+ * @author team4
+ */
+public class LocalWeatherView {
+	private String city;
+	private String country;
+	private WebInterface data;
+	private JSONObject object;
+	private JSONObject main;
+	private JSONObject wind;
+	private JSONObject sys;
+	private JSONArray weatherArray;
+	private JSONObject weather;
+
+	//constructor 1
+	public LocalWeatherView(String cityName) throws JSONException, IOException {
+		city = cityName;
+		data = new WebInterface(city);
+		object = data.createJSONObject(data.getLocalWeatherURL());
+	}
+
+	//constructor 2
+	public LocalWeatherView(String cityName, String countryCode) throws JSONException, IOException {
+		city = cityName;
+		country = countryCode;
+		data = new WebInterface(city,country);
+		object = data.createJSONObject(data.getLocalWeatherURL());
+	}
+
+	///////////////////////////HELPER METHODS///////////////////////
+    private JSONObject getMain() throws JSONException, IOException {
+		main = object.optJSONObject("main");
+		return main;
+    }
+
+    private JSONObject getSys() throws JSONException, IOException {
+		sys = object.optJSONObject("sys");
+		return sys;
+    }
+
+    private JSONObject getWind() throws JSONException, IOException {
+		wind = object.optJSONObject("wind");
+		return wind;
+    }
+
+    private JSONObject getWeather() throws JSONException, IOException {
+    	weatherArray = object.optJSONArray("weather");
+		weather = weatherArray.getJSONObject(0);
+		return weather;
+    }
+    ///////////////////////////HELPER METHODS///////////////////////
+
+    //GETTERS
+    public String getDate() throws JSONException, IOException {
+    	int dt = object.optInt("dt");
+    	Date date = new Date(dt * 1000L);
+        return date.toString();
+	}
+
+    public String getCityName() throws JSONException, IOException {
+		return object.optString("name");
+	}
+
+    public String getCountry() throws JSONException, IOException {
+		return getSys().optString("country", null);
+	}
+
+	/**
+	 * getTemperature method to return temperature
+	 * @return string temperature
+	 */
+	public String getTemperature() throws JSONException, IOException {
+	    DecimalFormat f = new DecimalFormat("0.0");
+	    double temp = getMain().optDouble("temp");
+	    if(GUIApp.pref.getUnit().equals("metric"))
+	    	return f.format(temp) + " \u00b0C";
+    	else
+    		return f.format(temp) + " \u00b0F";
+	}
+
+	/** getPressure method to return pressure
+	 *  @return strong pressure
+	 */
+	public String getPressure() throws JSONException, IOException {
+		return getMain().optString("pressure", null) + " hpa";
+	}
+
+	/** getHumidity method to return humidity
+	 *  @return humidity value
+	 */
+	public String getHumidity() throws JSONException, IOException {
+		return getMain().optString("humidity", null) + " %";
+	}
+
+	/** getTempMin method to return min Temp
+	 *  @return TempMin
+	 */
+	public String getTempMin() throws JSONException, IOException {
+		if(GUIApp.pref.getUnit().equals("metric"))
+			return getMain().optString("temp_min", null) + " \u00b0C";
+    	else
+    		return getMain().optString("temp_min", null) + " \u00b0F";
+	}
+
+	/** getTempMax method to return Max temp
+	 *  @return GetTempMax
+	 */
+	public String getTempMax() throws JSONException, IOException {
+		if(GUIApp.pref.getUnit().equals("metric"))
+			return getMain().optString("temp_max", null) + " \u00b0C";
+    	else
+    		return getMain().optString("temp_max", null) + " \u00b0F";
+	}
+
+	/** getWindSpeed method to return wind speed
+	 *  @return the value of windspeed
+	 */
+	public String getWindSpeed() throws JSONException, IOException {
+		return getWind().optString("speed", null) + " m/s";
+	}
+
+	/** getWindDirection method to return wind direction
+	 *  @return the value of wind direction
+	 */
+	public String getWindDirection() throws JSONException, IOException {
+		double degree = getWind().getDouble("deg");
+		String letters[] = { "N", "NE", "E", "SE", "S", "SW", "W", "NW", "N" };
+		return letters[(int)Math.round((degree / 45))];
+	}
+
+	/** getSunrise method to return sunrise time
+	 *  @return the value of sunrise
+	 */
+	public String getSunrise() throws JSONException, IOException {
+		Date date = new Date(getSys().optInt("sunrise") * 1000L);
+        return date.toString();
+	}
+
+	/** getSunset method to return sunset time
+	 *  @return the string sunset
+	 */
+	public String getSunset() throws JSONException, IOException {
+		Date date = new Date(getSys().optInt("sunset") * 1000L);
+        return date.toString();
+	}
+
+	/** getSkyCondition method to return Sky condition
+	 *  @return the string sky condition
+	 */
+	public String getSkyCondition() throws JSONException, IOException {
+		return getWeather().optString("main", null);
+	}
+
+    // TODO Remove this main method.
+	public static void main(String[] args) throws JSONException, IOException {
+		LocalWeatherView test = new LocalWeatherView("mississauga");
+		System.out.println("Date:\t" + test.getDate());
+		System.out.println("City:\t" + test.getCityName());
+		System.out.println("Country:\t" + test.getCountry());
+		System.out.println("Temperature:\t" + test.getTemperature());
+		System.out.println("Pressure:\t" + test.getPressure());
+		System.out.println("Humidity:\t" + test.getHumidity());
+		System.out.println("Sunrise:\t" + test.getSunrise());
+		System.out.println("Sunset:\t\t" + test.getSunset());
+		System.out.println("TempMax:\t" + test.getTempMax());
+		System.out.println("TempMin:\t" + test.getTempMin());
+		System.out.println("Wind Speed:\t" + test.getWindSpeed());
+		System.out.println("Wind Direction:\t" + test.getWindDirection());
+		System.out.println("Sky Condition:\t" + test.getSkyCondition());
+	}
+}
---
+++ Users/Leon/V1.7/WebInterface.java
@@ -0,0 +1,149 @@
+package ca.uwo.csd.cs2212.team4;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.*;
+
+public class WebInterface {
+	private static final String LOCAL_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?";
+	private static final String SHORT_TERM_URL = "http://api.openweathermap.org/data/2.5/forecast?";
+	private static final String LONG_TERM_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+	private static final String APPID = "appId=f74197d83bc45827574fcf77670f8a63";
+	private static final String MARS_URL = "http://marsweather.ingenology.com/v1/latest/?format=json";
+
+	private String cityName; 
+	private String countryCode;
+
+	public WebInterface(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public WebInterface(String cityName, String countryCode) {
+		this.cityName = cityName;
+		this.countryCode = countryCode;
+	}
+
+	public WebInterface() {
+	
+	}
+
+    // TODO added the url building part of the units, i believe everything is right.
+	public String buildLocalWeatherURL() throws UnsupportedEncodingException {
+        StringBuilder url = new StringBuilder();
+
+        url.append(LOCAL_WEATHER_URL);
+        if(GUIApp.pref.getUnit().equals("metric"))
+            url.append("units=metric&q=");
+        else if(GUIApp.pref.getUnit().equals("imperial"))
+            url.append("units=imperial&q=");     
+        else
+            System.out.println("Invalid unit.");
+
+        url.append(URLEncoder.encode(this.cityName, "UTF-8"));
+
+        if (this.countryCode != null) {
+            url.append(",").append(countryCode).append("&").append(APPID);
+		}
+        else {
+            url.append("&").append(APPID);
+        }
+
+        return url.toString();
+	}
+
+	public String getLocalWeatherURL() throws IOException{
+		return getJSON(buildLocalWeatherURL());
+	}
+
+	public String buildShortTermURL() throws UnsupportedEncodingException {
+        StringBuilder url = new StringBuilder();
+
+        url.append(SHORT_TERM_URL);
+
+        if(GUIApp.pref.getUnit().equals("metric"))
+            url.append("units=metric&q=");
+        else if(GUIApp.pref.getUnit().equals("imperial"))
+            url.append("units=imperial&q=");
+        else
+            System.out.println("Invalid unit.");
+
+        url.append(URLEncoder.encode(this.cityName, "UTF-8"));
+		if (this.countryCode != null) {
+			url.append(",").append(countryCode).append("&").append(APPID);
+		}
+        else {
+            url.append("&").append(APPID);
+        }
+
+        return url.toString();
+	}
+
+	public String getShortTermURL() throws IOException{
+		return getJSON(buildShortTermURL());
+	}
+
+	// TODO i wasn't able to test this method, we might need to take a look at it.
+    public String buildLongTermURL() throws UnsupportedEncodingException {
+        StringBuilder url = new StringBuilder();
+
+        url.append(LONG_TERM_URL);
+
+        if(GUIApp.pref.getUnit().equals("metric"))
+            url.append("units=metric&");
+        else if(GUIApp.pref.getUnit().equals("imperial"))
+            url.append("units=imperial&");
+        else
+            System.out.println("Invalid unit.");
+
+        url.append("cnt=10&mode=json&q=");
+
+        if (this.countryCode != null) {
+			url.append(",").append(countryCode).append("&").append(APPID);
+		}
+        else {
+            url.append("&").append(APPID);
+        }
+
+        return url.toString();
+	}
+
+	public String getLongTermURL() throws IOException{
+		return getJSON(buildLongTermURL());
+	}
+
+	public String buildMarsURL() throws UnsupportedEncodingException {
+		return new StringBuilder().append(MARS_URL).toString();
+	}
+
+	public String getMarsURL() throws IOException{
+		return getJSON(buildMarsURL());
+	}
+
+	public JSONObject createJSONObject(String stringNotParsed) throws JSONException {
+		JSONObject jsonObject;
+		if(stringNotParsed != null)
+			jsonObject = new JSONObject(stringNotParsed);
+		else
+			jsonObject = null;
+		return jsonObject;
+	}
+
+	public String getJSON(String urlToRead) throws IOException {
+		URL url = new URL(urlToRead);
+		HttpURLConnection request = (HttpURLConnection) url.openConnection();
+		BufferedReader in;
+		String line;
+		String result = "";
+
+		in = new BufferedReader(new InputStreamReader(request.getInputStream()));
+		while ((line = in.readLine()) != null) {
+			result += line;
+		}
+		in.close();
+		return result;
+	}
+}
