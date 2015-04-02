1a31b7db0cfdb684123421a7aaf6ab054abf697e
Taha Nasir <tnasir3@uwo.ca>
2015-04-01 21:16:36 -0400

    remove extra semicolon in GUICurrentWeather

--- src/main/java/ca/uwo/csd/cs2212/team4/GUICurrentWeather.java
+++ src/main/java/ca/uwo/csd/cs2212/team4/GUICurrentWeather.java
@@ -1,6 +1,6 @@
 package ca.uwo.csd.cs2212.team4;
 
-import java.awt.*;;
+import java.awt.*;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.*;
